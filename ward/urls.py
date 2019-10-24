ef0dc245bb243f248f7c170c46801451865c041a
Julia Agasaro <agasarojulia@gmail.com>
2019-10-24 14:59:39 +0200

    Added login,registration,signup html

--- ward/urls.py
+++ ward/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^award/',include('award.urls'))
+    url(r'^home/',include('award.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
